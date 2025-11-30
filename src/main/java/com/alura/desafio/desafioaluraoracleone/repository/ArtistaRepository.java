34c71b6f5e0e87e4b2d5ca62ffb24fd8b799a480
JoseRomualdo <jromualdo3@hotmail.com>
2025-11-30 10:41:04 -0300

    refactor(ArtistaRepository): add findByNomeComMusicas.

--- src/main/java/com/alura/desafio/desafioaluraoracleone/repository/ArtistaRepository.java
+++ src/main/java/com/alura/desafio/desafioaluraoracleone/repository/ArtistaRepository.java
@@ -2,6 +2,8 @@ package com.alura.desafio.desafioaluraoracleone.repository;
 
 import com.alura.desafio.desafioaluraoracleone.model.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,7 @@ import java.util.Optional;
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
 
     Optional<Artista> findByNome(String nome);
+
+    @Query("SELECT a FROM Artista a LEFT JOIN FETCH a.musicas WHERE a.nome = :nome")
+    Optional<Artista> findByNomeComMusicas(String nome);
 }
