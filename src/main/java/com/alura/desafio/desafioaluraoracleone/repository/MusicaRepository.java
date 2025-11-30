9b8e695c89af3542ae53093388cbcb8586aac5aa
JoseRomualdo <jromualdo3@hotmail.com>
2025-11-30 10:41:32 -0300

    refactor(MusicaRepository): add findByTitulo.

--- src/main/java/com/alura/desafio/desafioaluraoracleone/repository/MusicaRepository.java
+++ src/main/java/com/alura/desafio/desafioaluraoracleone/repository/MusicaRepository.java
@@ -4,6 +4,11 @@ import com.alura.desafio.desafioaluraoracleone.model.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MusicaRepository extends JpaRepository<Musica, Long> {
+
+    Optional<Musica> findByTitulo(Musica musica);
+
 }
